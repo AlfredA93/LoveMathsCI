cd3598bc07e7b7d9896bfcc19c94134de43165da
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-06-11 13:38:10 +0200

    Add division operations

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,7 +44,9 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
-    } else {
+    } else if (gameType === "division") {
+		displayDivisionQuestion(num1, num2);
+	} else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`
     }
@@ -89,7 +91,9 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if(operator === "-") { 
         return [operand1 - operand2, "subtract"];
-    } else {
+    } else if (operator ==="/") {
+		return [operand1 / operand2, "division"];
+	} else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
@@ -134,4 +138,15 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
+}
+
+/**
+ * Creates a Division Question that has an answer that is always a whole number
+ * as its answer by multiplying together operand1 & 2 to get the first operand.
+ */
+
+function displayDivisionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1*operand2;
+	document.getElementById("operand2").textContent = operand2
+	document.getElementById("operator").textContent = "/";
 }
